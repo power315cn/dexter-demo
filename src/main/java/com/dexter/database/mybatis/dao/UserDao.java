2a6ff1f899c716b1260805e7303667005428dfac
dexter <chengnan_520@163.com>
2016-04-22 18:05:28 +0800

    mybatis测试

---
+++ src/main/java/com/dexter/database/mybatis/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.dexter.database.mybatis.dao;
+
+import com.dexter.database.mybatis.domain.User;
+
+public interface UserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/dexter/database/mybatis/domain/User.java
@@ -0,0 +1,85 @@
+package com.dexter.database.mybatis.domain;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer sex;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
